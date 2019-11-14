9225086c6f1a0b918b0e6183471334f283a0d908
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-13 19:04:28 -0500

    create blank "lists" components

---
+++ src/components/lists/ListItem.js
@@ -0,0 +1,14 @@
+/// external modules ///
+import React from 'react';
+
+/***************************************
+  COMPONENT
+***************************************/
+function ListItem (props) {
+  return (
+    <div></div>
+  );
+};
+
+/**************************************/
+export default ListItem;
