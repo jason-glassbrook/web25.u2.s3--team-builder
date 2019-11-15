c7c471b48ba17d150b2be2754d1035dae6ff38e4
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-14 23:02:23 -0500

    import default data

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import React from 'react';
 /// styles ///
 import './styles/App.css';
 
+/// data ///
+import { protoPerson , listOfPersons } from './data/persons';
+import { protoTeam , listOfTeams } from './data/teams';
+
 /***************************************
   STATES
 ***************************************/
-const init = {};
+const init = {
+  'persons' : listOfPersons,
+  'person' : protoPerson,
+  'teams' : listOfTeams,
+  'team' : protoTeam,
+};
 
 /***************************************
   COMPONENT
